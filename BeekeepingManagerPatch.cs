a3e0a80b34323ef3871c838065333c00144ff75f
Snowz666 <zhaomingyu12345@gmail.com>
2025-09-27 05:22:25 +0800

    添加项目文件。

---
+++ BeekeepingManagerPatch.cs
@@ -0,0 +1,40 @@
+﻿using BokuMono;
+using BokuMono.Data;
+using HarmonyLib;
+using Il2CppSystem.Collections.Generic;
+using HoneyMustBeRoyal;
+
+[HarmonyPatch(typeof(BeekeepingManager))]
+public class BeekeepingManagerPatch
+{
+
+    [HarmonyPatch("AddCollectableFlowerInfo")]
+    [HarmonyPrefix]
+    static void PrefixAddCollectableFlowerInfo(ref BeekeepingManager.HarvestFlowerInfo info)
+    {
+        // It's royal when random value is greater than 32.
+        if (info.RandomValue > 32)
+        {
+            HoneyPlugin.Log.LogDebug($"Modify RandomValue : {info.RandomValue} -> 1.");
+            info.RandomValue = 1;
+        }
+    }
+    
+
+    [HarmonyPatch("TryCollectHoneycombFromFlowerInfo")]
+    [HarmonyPostfix]
+    static void PostfixTryCollectHoneycomb(BeekeepingManager.HarvestFlowerInfo info, ref Dictionary<uint, List<ItemData>> itemDataDic, ref List<ItemData> extras, ref bool __result)
+    {
+        __result = true;
+        
+        if (extras != null)
+        {
+            HoneyPlugin.Log.LogDebug($"RandomValue={info.RandomValue}, Result: {__result}, Royal Count = {extras.Count}");
+        }
+        else
+        {
+            HoneyPlugin.Log.LogWarning($"Variable 'extras' is null! ");
+        }
+        
+    }
+}
---
+++ HoneyMustBeRoyal.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+using HarmonyLib;
+
+namespace HoneyMustBeRoyal;
+
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class HoneyPlugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    private Harmony _harmony;
+    public override void Load()
+    {
+        Log = base.Log;
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll();
+
+        try
+        {
+            _harmony.PatchAll();
+            Log.LogDebug("Apply Harmony patch successed.");
+        }
+        catch (System.Exception ex)
+        {
+            Log.LogError($"Apply Harmony patch failed: {ex.Message}");
+            Log.LogDebug($"Exception msg: {ex}");
+        }
+
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
